0de42e49a7d4c5b05c4a0da6dc8642c213039a92
Hugo AVRIL <hugoavril2910@gmail.com>
2023-04-04 16:36:48 +0200

    sortie de fonction si null

--- src/main/java/com/genericite_part2/TableauAlg.java
+++ src/main/java/com/genericite_part2/TableauAlg.java
@@ -2,6 +2,8 @@ package com.genericite_part2;
 
 public class TableauAlg {
     public static <type extends Comparable<type>> Paire<type> minMax(type[] tab) {
+        if (tab.length == 0)
+            return null;
         type min = tab[0];
         type max = tab[0];
         for (type element : tab) {
